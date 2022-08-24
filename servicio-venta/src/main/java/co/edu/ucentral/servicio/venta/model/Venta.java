03ceaa0b70b04679c2bf17ba39d17a2855a5c13d
cesar velasco <caacesar.ariza@gmail.com>
2022-08-23 22:13:45 -0500

    Cambios de productos
    Cambios de productos y commons

---
+++ servicio-venta/src/main/java/co/edu/ucentral/servicio/venta/model/Venta.java
@@ -0,0 +1,73 @@
+package co.edu.ucentral.servicio.venta.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "ventas")
+public class Venta implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private int cantidad;
+	private Float valor;
+	@Temporal(TemporalType.DATE)
+	private Date fecha;
+	
+	@PrePersist
+	public void prePersist() {
+		fecha =new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Float getValor() {
+		return valor;
+	}
+
+	public void setValor(Float valor) {
+		this.valor = valor;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	
+	
+	
+	
+
+}
